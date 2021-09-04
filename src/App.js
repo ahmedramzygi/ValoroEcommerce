ccae620ab0f0756622541b8091c5a6e8d982c715
ahmedramzygi <17P8229@eng.asu.edu.eg>
2021-09-04 15:43:25 +0200

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import {commerce }from './lib/commerce'
+import{Navbar} from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello</p>
-      <p>Ahmed</p>
+const App = () => {
+  const [products,setProducts]=useState([]);
+  const fetchProducts=async()=>{
+    const response =await commerce.products.list()// Promise products fetched from API
+  }
+  return ( 
+    <div>
+      <Navbar/>
+      
     </div>
-  );
+   );
 }
-
+ 
 export default App;
